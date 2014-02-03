233e74abcc4a70f573e199074f5184b30bdfe1d2
Scott Burns <scott.s.burns@gmail.com>
2014-02-02 23:42:09 -0500

    Fix py3k relative import error

--- seam/__init__.py
+++ seam/__init__.py
@@ -18,4 +18,6 @@ __author__ = 'Scott Burns <scott.s.burns@vanderbilt.edu>'
 __copyright__ = 'Copyright 2014 Vanderbilt University. All Rights Reserved'
 __version__ = '0.0'
 
-import freesurfer
+from . import freesurfer
+
+__all__ = ['freesurfer', ]
