4c2ba4d31304d5978fce43ab55433addc225d853
Scott Burns <scott.s.burns@gmail.com>
2014-04-02 15:52:05 -0500

    Add utility functions & tests

--- seam/util.py
+++ seam/util.py
@@ -14,3 +14,24 @@ if PY2:
     STRING_TYPE = basestring
 else:
     STRING_TYPE = str
+
+import os
+from string import digits, ascii_letters
+from random import choice
+
+total = digits + ascii_letters
+
+def get_tmp_filename(ext='out', basename='/tmp', fname_length=32):
+    fname = ''.join(choice(total) for _ in range(fname_length))
+    return os.path.join(basename, '{}.{}'.format(fname, ext))
+
+
+def wrap_with_xvfb(command, wait=5, server_args='-screen 0, 1600x1200x24'):
+    parts = ['xvfb-run',
+        '-a', # automatically get a free server number
+        '-f {}'.format(get_tmp_filename()),
+        '-e {}'.format(get_tmp_filename()),
+        '--wait={:d}'.format(wait),
+        '--server-args="{}"'.format(server_args),
+        command]
+    return ' '.join(parts)
