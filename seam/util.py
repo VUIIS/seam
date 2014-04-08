aebbb2d448f41f68cc4016f8585e46db41ba12d2
Scott Burns <scott.s.burns@gmail.com>
2014-04-08 09:05:58 -0500

    Add util tests

--- seam/util.py
+++ seam/util.py
@@ -9,22 +9,21 @@ __author__ = 'Scott Burns <scott.s.burns@vanderbilt.edu>'
 __copyright__ = 'Copyright 2014 Vanderbilt University. All Rights Reserved'
 
 import sys
+import os
+from string import digits, ascii_letters
+from random import choice
+
 PY2 = sys.version_info[0] == 2
 if PY2:
     STRING_TYPE = basestring
 else:
     STRING_TYPE = str
 
-import os
-from string import digits, ascii_letters
-from random import choice
-
 total = digits + ascii_letters
 
 def get_tmp_filename(ext='out', basename='/tmp', fname_length=32):
     fname = ''.join(choice(total) for _ in range(fname_length))
     return os.path.join(basename, '{}.{}'.format(fname, ext))
-
 
 def wrap_with_xvfb(command, wait=5, server_args='-screen 0, 1600x1200x24'):
     parts = ['xvfb-run',
--- tests/test_util.py
+++ tests/test_util.py
@@ -7,7 +7,7 @@ Test utility functions
 __author__ = 'Scott Burns <scott.s.burns@vanderbilt.edu>'
 __copyright__ = 'Copyright 2014 Vanderbilt University. All Rights Reserved'
 
-from seam.util import get_tmp_filename, wrap_with_xvfb
+from seam.util import get_tmp_filename, wrap_with_xvfb, total, STRING_TYPE, PY2
 
 def test_default_tmp_filename():
     cmp_filename = get_tmp_filename()
@@ -42,3 +42,17 @@ def test_wrap_with_xvfb_options():
     cmp_wrapper = wrap_with_xvfb('ls -l', wait=wait, server_args=server_args)
     assert server_args in cmp_wrapper
     assert '--wait={:d}'.format(wait) in cmp_wrapper
+
+def test_total():
+    assert len(total) == 62
+
+def test_string_type():
+    import sys
+    if sys.version_info[0] == 2:
+        assert STRING_TYPE == basestring
+    else:
+        assert STRING_TYPE == str
+
+def test_python_version():
+    import sys
+    assert (sys.version_info[0] == 2) == PY2
